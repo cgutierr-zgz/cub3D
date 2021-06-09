5bd4239d9a50bad332ceda7e3aa939144ab420f9
Carlos <carlosgutimo.3d@gmail.com>
2021-06-09 14:15:13 +0200

    :sparkles: Minimap
    Added new minimap system to show the players position

--- src_bonus/rc_aux_bonus.c
+++ src_bonus/rc_aux_bonus.c
@@ -3,26 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   rc_aux_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cgutierr <cgutierr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 18:57:20 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/04/26 18:58:11 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/06/09 14:14:11 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub_bonus.h"
 
+static void	draw_n_pixels(int n_pixels, t_cub *cub, int i, int i2)
+{
+	int	x;
+	int	x2;
+
+	x = 0;
+	while (x < n_pixels)
+	{
+		x2 = 0;
+		while (x2 < n_pixels)
+		{
+			my_pixel_put(&cub->txtrs.main_img, i + x, i2 + x2 + 125, cub);
+			x2++;
+		}
+		x++;
+	}
+}
+
+//FIXME: qu eno sea más grande que la pantalla
+typedef struct s_minimap
+{
+	int	i;
+	int	i2;
+	int	offsetx;
+	int	offsety;
+}	t_minimap;
+
+static void	minimap(t_cub *cub)
+{
+	t_minimap	m;
+
+	m.offsety = 0;
+	m.i = 0;
+	while (m.i < cub->map.map_lines)
+	{
+		m.i2 = 0;
+		m.offsetx = 0;
+		while (m.i2 < cub->map.max_len)
+		{
+			cub->data.color = 321123321;
+			if (cub->map.map_matrix[m.i][m.i2] == '1')
+				cub->data.color = 23132;
+			else if (cub->map.map_matrix[m.i][m.i2] == '0')
+				cub->data.color = 45324321;
+			if ((int)cub->raycast.posx == m.i && (int)cub->raycast.posy == m.i2)
+				cub->data.color = create_trgb(0, 255, 0, 0);
+			if (cub->map.map_matrix[m.i][m.i2] != ' ')
+				draw_n_pixels(4, cub, m.i2 + m.offsetx, m.i + m.offsety);
+			m.i2++;
+			m.offsetx += 4;
+		}
+		m.offsety += 4;
+		m.i++;
+	}
+}
+
 void	hud_bonus(t_cub *cub)
 {
 	if (cub->attack)
 	{
-		mlx_put_image_to_window(cub->mlx, cub->window,
-			cub->txtrs.img[14].img, cub->screen.x / 2, cub->screen.y - 200);
+		mlx_put_image_to_window(cub->mlx, cub->window, cub->txtrs.img[14].img,
+			cub->screen.x / 2, cub->screen.y - 200);
 		cub->attack = 0;
 	}
 	else
-		mlx_put_image_to_window(cub->mlx, cub->window,
-			cub->txtrs.img[13].img, cub->screen.x / 2, cub->screen.y - 200);
+		mlx_put_image_to_window(cub->mlx, cub->window, cub->txtrs.img[13].img,
+			cub->screen.x / 2, cub->screen.y - 200);
+	minimap(cub);
 }
 
 void	vida_hud(t_cub *cub)
@@ -45,8 +102,8 @@ void	vida_hud(t_cub *cub)
 	free(num_enemyj);
 	vidas = -1;
 	while (++vidas < cub->vida)
-		mlx_put_image_to_window(cub->mlx,
-			cub->window, cub->txtrs.img[7].img, 10 + (vidas * 20), 50);
+		mlx_put_image_to_window(cub->mlx, cub->window, cub->txtrs.img[7].img, 10
+			+ (vidas * 20), 50);
 	if (cub->num_enemy == 0 && cub->won == 0)
 	{
 		printf("\nYou killed them all!\nYou can keep on playing\n");
